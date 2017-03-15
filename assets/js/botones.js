f289fa6c830baca351d4e2e769e83f46408048ab
LeidyRosmery <rosm3rita@hotmail.com>
2017-03-15 02:17:40 -0500

    funciona los botones

--- assets/js/botones.js
+++ assets/js/botones.js
@@ -1,8 +1,17 @@
-var mostarOcultar = function(divtexte) {
-	var divtext = document.getElementById(divtexte);
-	if (divtext.style.display == "block" ) {
-		divtext.style.display = "none";
-	} else{
-		divtext.style.display = "block";
-	}
+var sprint1=document.getElementById("sprint1");
+var sprint2=document.getElementById("sprint2");
+var evaluacion1=document.getElementById("evaluacion1");
+var evaluacion2=document.getElementById("evaluacion2");
+
+var mostarOcultar = function() {
+	sprint1.style.display = "block";
+	evaluacion1.style.display = "block";
+	sprint2.style.display = "none";
+	evaluacion2.style.display = "none";
+}
+var mostarOcultar2 = function() {
+	sprint1.style.display = "none";
+	evaluacion1.style.display = "none";
+	sprint2.style.display = "block";
+	evaluacion2.style.display = "block";
 }
